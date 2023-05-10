bbfd9015548f30cbc2d838c4d7b9f9733cda0382
tuyenhn <hntuyen282@gmail.com>
2023-05-10 17:44:32 +0700

    add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import "../styles/globals.css";
+import localFont from "next/font/local";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const foundrySterling = localFont({
+  src: [
+    {
+      path: "/fonts/FoundrySterling-BookItalic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "/fonts/FoundrySterling-Demi.otf",
+      weight: "400",
+    },
+    {
+      path: "/fonts/FoundrySterling-ExtraBold.otf",
+      weight: "800",
+    },
+    {
+      path: "/fonts/FoundrySterling-Light.otf",
+      weight: "300",
+    },
+    {
+      path: "/fonts/FoundrySterling-Medium.otf",
+      weight: "500",
+    },
+  ],
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={foundrySterling.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
-
-export default MyApp;
