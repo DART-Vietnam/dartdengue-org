4c2ee6fb9333bc2f327ec26f8070c422a027665e
tuyenhn <hntuyen282@gmail.com>
2023-05-19 16:13:13 +0700

    pnpm packages and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/**/*.js"
   ],
   theme: {
     extend: {
@@ -12,11 +13,18 @@ module.exports = {
       },
       backgroundImage: {
         'dart-logo': "url('/dart_oucru.png')",
+        'dart-kom1': "url('/dart_kom1.jpg')",
+        'dart-kom2': "url('/dart_kom2.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+  ],
 };
