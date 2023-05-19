db18da34750f882458a04441bb16b4ad05733587
tuyenhn <hntuyen282@gmail.com>
2023-05-19 16:12:16 +0700

    change navbar style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,30 +1,92 @@
-import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "Team and partners", href: "/team" },
+  { name: "Reports", href: "#" },
+  { name: "Get involved", href: "#" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
-    <div className="absolute top-0 left-0 z-20 w-full h-20 p-3 bg-transparent sm:px-5 group-hover:bg-white">
-      <div className="container grid items-center h-full grid-cols-2">
-        <div className="flex items-center h-full">
-          <div className="w-1/6 h-full mr-5 bg-center bg-no-repeat bg-contain bg-dart-logo" />
-          <div className="self-center text-xl font-bold select-none lg:text-2xl text-oxfordBlue whitespace-nowrap">
-            Dengue Advanced Readiness Tools
-          </div>
-        </div>
-        <div className="items-center justify-center hidden w-full md:flex md:w-auto">
-          <div className="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <div>
-              <a href="/" className="navbar-text-current">
-                Home
-              </a>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-20">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block w-6 h-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block w-6 h-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start h-5/6">
+                <div className="flex items-center flex-shrink-0">
+                  <div className="w-24 h-full bg-center bg-no-repeat bg-contain lg:hidden bg-dart-logo"></div>
+                  <div className="hidden w-24 h-full bg-center bg-no-repeat bg-contain lg:block bg-dart-logo"></div>
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex h-full space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          router.pathname == item.href
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "px-3 py-2 rounded-md font-bold self-center"
+                        )}
+                        aria-current={
+                          router.pathname == item.href ? "page" : undefined
+                        }
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
-            <div>
-              <a href="/" className="navbar-text">
-                Our Team
-              </a>
-            </div>
           </div>
-        </div>
-      </div>
-    </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    router.pathname == item.href
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={
+                    router.pathname == item.href ? "page" : undefined
+                  }
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
