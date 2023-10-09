df157a7883f591a063b3fcf8a047e3dd5ac50d1d
tuyenhn <hntuyen282@gmail.com>
2023-10-09 14:44:21 +0700

    refactor news list

---
+++ components/news-list.tsx
@@ -0,0 +1,36 @@
+
+const posts = [
+    {
+        title: "Dengue Advanced Readiness Tools (DART)",
+        href: "https://www.oucru.org/project/dengue-advanced-readiness-tools-dart/",
+        description:
+            "This project explores if and how remote climatic drivers may affect dengue outbreaks in Viet Nam, leading to a greater understanding of weather and climate ...",
+        date: "Mar 16, 2020",
+        datetime: "2020-03-16",
+        imageUrl:
+            "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    },
+    {
+        title:
+            "New multidisciplinary project will help forecast where and when deadly disease outbreaks are likely to occur",
+        href: "https://www.ox.ac.uk/news/2023-02-03-new-multidisciplinary-project-will-help-forecast-where-and-when-deadly-disease",
+        description:
+            "DART (Dengue Advanced Readiness Tools), a new project led by Oxford University, has received funding from Wellcome to use climate data to ...",
+        date: "Feb 3, 2023",
+        datetime: "2023-02-03",
+        imageUrl:
+            "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    },
+    {
+        title: " Multidisciplinary dengue forecasting project launches in Vietnam ",
+        href: "https://www.ndm.ox.ac.uk/news/multidisciplinary-dengue-forecasting-project-launches-in-vietnam",
+        description:
+            "DART (Dengue Advanced Readiness Tools), is a new project supported by Wellcome to use climate data to better predict and prepare for ...",
+        date: "Feb 7, 2023",
+        datetime: "2023-02-07",
+        imageUrl:
+            "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    },
+];
+
+export default posts;
