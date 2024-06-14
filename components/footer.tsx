bb59540109a868dd5250c80bffe7dce8961e12b1
tuyenhn <hntuyen282@gmail.com>
2024-06-14 11:06:38 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,51 +1,29 @@
+import Link from "next/link";
+
 const navigation = {
-  ourwork: [
-    { name: "About the project", href: "/" },
-    { name: "Meet the team", href: "/team" },
-    { name: "Methodology", href: "#" },
-    { name: "Partners", href: "#" },
-  ],
-  placeholder2: [
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-  ],
-  placeholder3: [
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-  ],
-  placeholder4: [
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-    { name: "x", href: "#" },
-  ],
   social: [
-    {
-      name: "Facebook",
-      href: "#",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Twitter",
-      href: "#",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-        </svg>
-      ),
-    },
+    // {
+    //   name: "Facebook",
+    //   href: "#",
+    //   icon: (props: any) => (
+    //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+    //       <path
+    //         fillRule="evenodd"
+    //         d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+    //         clipRule="evenodd"
+    //       />
+    //     </svg>
+    //   ),
+    // },
+    // {
+    //   name: "Twitter",
+    //   href: "#",
+    //   icon: (props: any) => (
+    //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+    //       <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+    //     </svg>
+    //   ),
+    // },
     {
       name: "GitHub",
       href: "https://github.com/DART-Vietnam",
@@ -69,44 +47,21 @@ export default function Footer() {
         Footer
       </h2>
       <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:py-8 lg:px-8">
-        {/* <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
-          <div className="grid grid-cols-2 gap-8 xl:col-span-4">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
-                  Our work
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  {navigation.ourwork.map((item) => (
-                    <li key={item.name}>
-                      <a
-                        href={item.href}
-                        className="text-base text-gray-300 hover:text-white"
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <div className="p-2 md:flex md:items-center md:justify-between">
           <div className="flex justify-center space-x-6 md:order-2">
             {navigation.social.map((item) => (
-              <a
+              <Link
                 key={item.name}
                 href={item.href}
                 className="text-gray-400 hover:text-gray-300"
               >
                 <span className="sr-only">{item.name}</span>
                 <item.icon className="w-6 h-6" aria-hidden="true" />
-              </a>
+              </Link>
             ))}
           </div>
           <p className="mt-8 text-base text-center text-gray-400 md:mt-0 md:order-1">
-            &copy; 2023 DART team.
+            Dengue Advanced Readiness Tools (DART) consortium.
           </p>
         </div>
       </div>
