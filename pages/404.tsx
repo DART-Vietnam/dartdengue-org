25b5126a1390af1d66f68c0f9e86eb5614ca9d5a
tuyenhn <hntuyen282@gmail.com>
2024-06-19 16:14:44 +0700

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 
-export default function Example() {
+export default function Custom404() {
   return (
-    <div className="bg-white min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-      <div className="max-w-max mx-auto">
+    <div className="min-h-screen px-4 py-16 bg-white sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="mx-auto max-w-max">
         <main className="sm:flex">
-          <p className="text-4xl font-extrabold text-oxfordBlue sm:text-5xl">404</p>
+          <p className="text-4xl font-extrabold text-oxfordBlue sm:text-5xl">
+            404
+          </p>
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-              <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found</h1>
-              <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-1 text-base text-gray-500">
+                Please check the URL and try again.
+              </p>
             </div>
-            <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <div className="flex mt-10 space-x-3 sm:border-l sm:border-transparent sm:pl-6">
               <Link
                 href="/"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-oxfordBlue hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2"
+                className="inline-flex items-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-oxfordBlue hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2"
               >
                 Go back home
               </Link>
@@ -23,5 +29,5 @@ export default function Example() {
         </main>
       </div>
     </div>
-  )
+  );
 }
