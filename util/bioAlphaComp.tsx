48ce4f2016e6875a8edf19d2fc854374963717fd
tuyenhn <hntuyen282@gmail.com>
2024-06-19 16:21:25 +0700

    formatting

--- util/bioAlphaComp.tsx
+++ util/bioAlphaComp.tsx
@@ -1,9 +1,5 @@
 import bio from "./bio";
 
 export default function bioAlphaComp(obj1: bio, obj2: bio) {
-    return (obj1.name < obj2.name) ?
-        -1 :
-        (obj1.name > obj2.name) ?
-            1 :
-            0;
+  return obj1.name < obj2.name ? -1 : obj1.name > obj2.name ? 1 : 0;
 }
