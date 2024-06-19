4c4895f16bec4f9113a90b0874257644f736789c
tuyenhn <hntuyen282@gmail.com>
2024-06-19 16:31:24 +0700

    Update bioAlphaComp.tsx

--- util/bioAlphaComp.tsx
+++ util/bioAlphaComp.tsx
@@ -1,4 +1,4 @@
-import bio from "./bio";
+import { bio } from "./interfaces";
 
 export default function bioAlphaComp(obj1: bio, obj2: bio) {
   return obj1.name < obj2.name ? -1 : obj1.name > obj2.name ? 1 : 0;
